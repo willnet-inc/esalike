100305f4c64b72e678bca068910a17481ff5de7c
Masatoshi Iwasaki <mstshiwasaki@gmail.com>
2022-12-25 17:58:21 +0900

    Optimize CI

--- apps/demo/jest-puppeteer.config.js
+++ apps/demo/jest-puppeteer.config.js
@@ -1,13 +1,12 @@
-// jest-puppeteer.config.js
 module.exports = {
   launch: {
     dumpio: true,
     headless: true,
     product: 'chrome',
   },
-  browserContext: 'default',
   server: {
     command: 'npm run test:serve',
-    port: 3001
+    port: 3001,
+    launchTimeout: 5000 * 5 // Default value(5000) was not enough on CI
   }
 }
